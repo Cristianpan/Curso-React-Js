19faec159a6f37f2fa2895df179e18af48cca95c
cristianpan <panzaldivarcristian@gmail.com>
2024-07-30 01:52:38 -0600

    seccion-15:finalizada
    videos:207-217

--- 06-heroes-spa/src/auth/pages/LoginPage.jsx
+++ 06-heroes-spa/src/auth/pages/LoginPage.jsx
@@ -1,12 +1,20 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context";
 
 export const LoginPage = () => {
   const navigate = useNavigate();
+  const {login} = useContext(AuthContext); 
 
   const handleLogin = () => {
-    navigate("/marvel", {
+    login('Cristian Pan'); 
+
+    const lastPath = localStorage.getItem('lastPath') || "/"; 
+
+    navigate(lastPath, {
       replace: true,
     });
+
   };
 
   return (
